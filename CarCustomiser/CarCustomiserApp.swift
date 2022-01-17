ca48665b429ee1b3a46e5d2862713342808780ea
Schuyler Audley-Williams <audley-williams.s@etoncollege.org.uk>
2022-01-17 20:04:03 +0000

    Initial Commit

---
+++ CarCustomiser/CarCustomiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarCustomiserApp.swift
+//  CarCustomiser
+//
+//  Created by Audley-Williams, Schuyler (SPH) on 17/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct CarCustomiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
